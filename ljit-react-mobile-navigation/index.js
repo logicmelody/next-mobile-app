dbb71721a091b6b0a4264b206018d7b5e7c35305
Danny Lin <danny.l@ljit.io>
2023-05-31 14:54:51 +0800

    Base startApp

---
+++ ljit-react-mobile-navigation/index.js
@@ -0,0 +1,76 @@
+import { Redirect, Route } from 'react-router-dom';
+import { IonReactRouter } from '@ionic/react-router';
+import {
+	IonApp,
+	IonRouterOutlet,
+} from '@ionic/react';
+
+import withPage from './with-page';
+
+function startApp({
+	tabs,
+	pages,
+	rootRoute,
+}) {
+	function _renderRoutes() {
+		return pages.map(page => {
+			const {
+				path,
+			} = page;
+
+			const WrapperPageComponent = withPage(page);
+
+			return (
+				<Route
+					key={path}
+					path={path}
+				>
+					<WrapperPageComponent />
+				</Route>
+			);
+		});
+	}
+
+	function _renderTabsRoute() {
+		if (tabs) {
+			return (
+				<Route path={TABS}>
+					{/* <Tabs /> */}
+				</Route>
+			);
+		}
+	}
+
+	return (
+		<IonApp>
+			<IonReactRouter>
+				{/* An IonRouterOutlet should only contain Routes or Redirects. */}
+				<IonRouterOutlet>
+					{_renderRoutes()}
+					{_renderTabsRoute()}
+
+					<Route exact path='/'>
+						<Redirect to={rootRoute} />
+					</Route>
+
+					{/* A common routing use case is to provide a "fallback" route to be rendered in the event the location navigated to does not match any of the routes defined. */}
+					{/* <Route
+							render={() => <Redirect to='/tab1' />}
+						/> */}
+
+					{/* <Route
+							exact
+							path="/dashboard"
+							render={(props) => {
+								return isAuthed ? <DashboardPage {...props} /> : <LoginPage />;
+							}}
+						/> */}
+				</IonRouterOutlet>
+			</IonReactRouter>
+		</IonApp>
+	);
+}
+
+export {
+	startApp,
+};
